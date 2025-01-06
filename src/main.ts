3ae22999c597b0ddf329d10303f0c2302c747c0f
Sebastian <sebastianvalbuena25@gmail.com>
2025-01-05 19:16:50 -0500

    launcher git

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function main() {
   const logger = new Logger('main');
 
-  console.log(envs.natsServers)
-
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
